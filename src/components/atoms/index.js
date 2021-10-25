63e8a237af954ea3a1497663990a79810e65b2f9
Ca10San <caiodarkus2@gmail.com>
2021-10-25 16:18:47 -0300

    fix(CMain): corrige lista de imports, falta CLogo

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -4,3 +4,4 @@ export { default as CCheckbox } from "./CCheckbox/CCheckbox.vue";
 export { default as CInput } from "./CInput/CInput.vue";
 export { default as CMessage } from "./CMessage/CMessage.vue";
 export { default as CVideo } from "./CVideo/CVideo.vue";
+export { default as CLogo } from "./CLogo/CLogo.vue";
