ea55cbf0e8466bfc0218ff8a058605765d4a21dc
Ca10San <caiodarkus2@gmail.com>
2021-11-23 18:27:37 -0300

    v0.4.2 - test(Cfinish): adds store injection

--- src/components/organisms/CFinish/CFinish.spec.js
+++ src/components/organisms/CFinish/CFinish.spec.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
-import { shallowMount } from '@vue/test-utils'
-import CFinish from './CFinish.vue'
+import { mount } from '@vue/test-utils';
+import CFinish from './CFinish.vue';
+import store from '@/store';
 
 describe('./CFinish.vue', () => {
   it('renders save options', () => {
-    const component = shallowMount(CFinish)
+    const component = mount(CFinish, { 
+      global: {
+        plugins: [store],
+      }
+    });
     expect(component.isVisible()).toBe(true)
   })
 })
