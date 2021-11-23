a358f7e4229d8fe1018e44a4380ef5101fc07f3d
Ca10San <caiodarkus2@gmail.com>
2021-11-23 18:21:32 -0300

    v0.4.2 - test(CSection): add store import

--- src/components/molecules/CSection/CSection.spec.js
+++ src/components/molecules/CSection/CSection.spec.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-undef */
 import { shallowMount } from "@vue/test-utils";
 import CSection from "./CSection.vue";
+import store from "@/store"
 
 describe("./CSection.vue", () => {
   it("renders a section container", () => {
     const component = shallowMount(CSection, {
+      global: {
+        plugins: [store]
+      },
       slots: {
         named: {
           info: "Section information",
