b5cab9e6132552fdf5c013dec9cea71007105c7a
Ca10San <caiodarkus2@gmail.com>
2021-11-23 18:36:05 -0300

    v0.4.2 - test(CSectionArrange): adds store injection

--- src/components/organisms/CSectionArrange/CSectionArrange.spec.js
+++ src/components/organisms/CSectionArrange/CSectionArrange.spec.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils';
 import CSectionArrange from './CSectionArrange.vue'
+import store from '@/store';
 
 describe('./CSectionArrange.vue', () => {
   it('renders section arrangement widget', () => {
-    const component = shallowMount(CSectionArrange)
+    const component = shallowMount(CSectionArrange, {
+      global: {
+        plugins: [store]
+      }
+    });
     expect(component.isVisible()).toBe(true)
   })
 })
