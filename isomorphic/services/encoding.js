b0ca4b221481efa6cbec278ecf70a9053961eb09
Ca10San <caiodarkus2@gmail.com>
2021-12-08 15:28:45 -0300

    v0.4.3 - Fix(encoding): remove deprecated atob/btoa methods

--- isomorphic/services/encoding.js
+++ isomorphic/services/encoding.js
@@ -1,11 +1,15 @@
-export const toBase64 = (s) => {
-  return typeof btoa !== "function"
-    ? new Buffer.alloc(s.length, s).toString("base64")
-    : btoa(s);
+export const toBase64 = (str) => {
+  if (typeof str === "string") {
+    let buff = Buffer.from(str, 'utf-8');
+    const encodedString = buff.toString('base64');
+    return encodedString;
+  }
 };
 
-export const fromBase64 = (s) => {
-  return typeof atob !== "function"
-    ? new Buffer(s, "base64").toString()
-    : atob(s);
+export const fromBase64 = (base64) => {
+  if (typeof base64 === "string") {
+    let buff = Buffer.from(base64, 'base64');
+    const decodedString = buff.toString('utf-8');
+    return decodedString;
+  }
 };
---
+++ isomorphic/services/encoding.spec.js
@@ -0,0 +1,11 @@
+import { fromBase64, toBase64 } from "./encoding";
+
+describe("Encoding", () => {
+    test("Sucessfull Encoding and Decoding" , () => {
+        const baseText = JSON.stringify({ prop:'value' });
+        const encodedString = toBase64(baseText);
+        const decodedString = fromBase64(encodedString);
+        console.log(encodedString, decodedString);
+        expect(decodedString).toEqual(baseText);
+    })
+});
